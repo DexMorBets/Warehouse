bcd56f522ebee711334956e287c236e1351955b2
DexMor <dexmor93@gmail.com>
2017-05-22 23:42:13 +0300

    dasdasd

---
+++ static/js/Own/min/showMenu.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$("i.fa").tap(function(){$(this).toggleClass("fa-times").toggleClass("fa-bars"),$(".menu-hidden").toggleClass("show")})});
---
+++ static/js/Own/showMenu.js
@@ -0,0 +1,6 @@
+$(document).ready( function () {
+    $('i.fa').tap( function () {
+        $(this).toggleClass('fa-times').toggleClass('fa-bars')
+        $('.menu-hidden').toggleClass('show');
+    });
+});
