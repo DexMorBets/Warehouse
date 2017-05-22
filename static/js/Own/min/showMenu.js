7a9deab7a8ca39f330bdf7be618a50c5d24dca3e
DexMor <dexmor93@gmail.com>
2017-05-23 02:24:28 +0300

    dasdasd

--- static/js/Own/min/showMenu.js
+++ static/js/Own/min/showMenu.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$("i.fa").tap(function(){$(this).toggleClass("fa-times").toggleClass("fa-bars"),$(".menu-hidden").toggleClass("show")})});
+$(document).ready(function(){$(".navbar-block i.fa").tap(function(){$(this).toggleClass("fa-times").toggleClass("fa-bars"),$(".menu-hidden").toggleClass("show")})});
--- static/js/Own/showMenu.js
+++ static/js/Own/showMenu.js
@@ -1,5 +1,5 @@
 $(document).ready( function () {
-    $('i.fa').tap( function () {
+    $('.navbar-block i.fa').tap( function () {
         $(this).toggleClass('fa-times').toggleClass('fa-bars')
         $('.menu-hidden').toggleClass('show');
     });
