b23c57477e1cb3efe2ab5759beeacee94a79b38d
DexMor <dexmor93@gmail.com>
2017-05-23 04:20:56 +0300

    dasdasd

--- static/js/Own/currentPage.js
+++ static/js/Own/currentPage.js
@@ -7,6 +7,6 @@ $(document).ready(function () {
     } else if (page == "3"){
         $(".plan_li").css('border-bottom','5px solid #ffa500')
     } else if (page == "4"){
-        $(".header_avatar_img").css({'border':'3px solid #ffa500','width':'46px','height':'46px','margin-top':'11px'})
+        $(".header_avatar_img").css({'border':'3px solid #ffa500'})
     };
 });
